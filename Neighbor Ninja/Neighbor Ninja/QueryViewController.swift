9a95c726274cc05e3b74bbb0f3fa9e7c83bd9e88
gameriot <s.v.runescapefan@gmail.com>
2016-07-01 22:42:12 -0700

    working tab bar

--- Neighbor Ninja/Neighbor Ninja/QueryViewController.swift
+++ Neighbor Ninja/Neighbor Ninja/QueryViewController.swift
@@ -29,43 +29,43 @@ class QueryViewController: UITableViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let items = ["Home", "Report", "View", "Settings", "Sign Off"]
-//        self.selectedCellLabel.text = items.first
-//        self.navigationController?.navigationBar.translucent = false
-//        self.navigationController?.navigationBar.barTintColor = UIColor(red: 255.0/255.0, green:100.0/255.0, blue:190.0/255.0, alpha: 1.0)
-//        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-//        
-//        menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, title: items[2], items: items)
-//        menuView.cellHeight = 50
-//        menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
-//        menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green:160.0/255.0, blue:195.0/255.0, alpha: 1.0)
-//        menuView.keepSelectedCellColor = true
-//        menuView.cellTextLabelColor = UIColor.whiteColor()
-//        menuView.cellTextLabelFont = UIFont(name: "Avenir-Heavy", size: 17)
-//        menuView.cellTextLabelAlignment = .Left // .Center // .Right // .Left
-//        menuView.arrowPadding = 15
-//        menuView.animationDuration = 0.5
-//        menuView.maskBackgroundColor = UIColor.blackColor()
-//        menuView.maskBackgroundOpacity = 0.3
-//        menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
-//            print("Did select item at index: \(indexPath)")
-//            self.selectedCellLabel.text = items[indexPath]
-//            if self.selectedCellLabel.text == "Report"{
-//                self.performSegueWithIdentifier("viewtoreport", sender: self)
-//            }
-//            if self.selectedCellLabel.text == "Home"{
-//                self.performSegueWithIdentifier("viewtohome", sender: self)
-//            }
-//            if self.selectedCellLabel.text == "Settings"{
-//                
-//            }
-//            if self.selectedCellLabel.text == "Sign Off"{
-//                NSUserDefaults.standardUserDefaults().setBool(false, forKey:"isUserLoggedIn")
-//                NSUserDefaults.standardUserDefaults().synchronize()
-//                self.performSegueWithIdentifier("loginView", sender: self)
-//            }
-//        }
-//        self.navigationItem.titleView = menuView
+        let items = ["Home", "Report", "View", "Settings", "Sign Off"]
+        self.selectedCellLabel.text = items.first
+        self.navigationController?.navigationBar.translucent = false
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 255.0/255.0, green:100.0/255.0, blue:190.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, title: items[2], items: items)
+        menuView.cellHeight = 50
+        menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
+        menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green:160.0/255.0, blue:195.0/255.0, alpha: 1.0)
+        menuView.keepSelectedCellColor = true
+        menuView.cellTextLabelColor = UIColor.whiteColor()
+        menuView.cellTextLabelFont = UIFont(name: "Avenir-Heavy", size: 17)
+        menuView.cellTextLabelAlignment = .Left // .Center // .Right // .Left
+        menuView.arrowPadding = 15
+        menuView.animationDuration = 0.5
+        menuView.maskBackgroundColor = UIColor.blackColor()
+        menuView.maskBackgroundOpacity = 0.3
+        menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
+            print("Did select item at index: \(indexPath)")
+            self.selectedCellLabel.text = items[indexPath]
+            if self.selectedCellLabel.text == "Report"{
+                self.performSegueWithIdentifier("viewtoreport", sender: self)
+            }
+            if self.selectedCellLabel.text == "Home"{
+                self.performSegueWithIdentifier("viewtohome", sender: self)
+            }
+            if self.selectedCellLabel.text == "Settings"{
+                
+            }
+            if self.selectedCellLabel.text == "Sign Off"{
+                NSUserDefaults.standardUserDefaults().setBool(false, forKey:"isUserLoggedIn")
+                NSUserDefaults.standardUserDefaults().synchronize()
+                self.performSegueWithIdentifier("loginView", sender: self)
+            }
+        }
+        self.navigationItem.titleView = menuView
 
 
         // Create a pull-to-refresh control
