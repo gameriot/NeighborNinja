0d123c077e952f0a5632ea2565e5ca2708583881
Samast Varma <samastv99@gmail.com>
2017-02-25 22:53:53 -0800

    scrollview and updated home

--- Neighbor Ninja/Neighbor Ninja/AppDelegate.swift
+++ Neighbor Ninja/Neighbor Ninja/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import UserNotifications
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -34,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         else {  
             application.registerForRemoteNotifications(matching: [.badge, .sound, .alert])
         }
+        IQKeyboardManager.sharedManager().enable = true
         return true;
     }
     
